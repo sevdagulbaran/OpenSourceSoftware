165c587249113a0b0a8aa7475a768c8df4713b1f
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-10-10 00:54:56 +0300

    Added first assignment.
    init project in xcode

---
+++ OpenSourceSoftware/OpenSourceSoftware/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  OpenSourceSoftware
+//
+//  Created by Sevda Gul Baran on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print(" First assignment in course..")
+    }
+}
+
