487f1e671f99bbcaa352eaadabe023f59006a63f
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-10-16 02:57:39 +0300

    Added Rest Api.

---
+++ Rest API/Rest API/Controller/MovieLogic.swift
@@ -0,0 +1,41 @@
+//
+//  MovieLogic.swift
+//  Rest API
+//
+//  Created by Sevda Gul Baran on 16.10.2022.
+//
+
+import Foundation
+
+class MovieLogic {
+    
+    private var apiService = ApiService()
+    private var popularMovies = [Movie]()
+    
+    func fetchPopularMoviesData(completion: @escaping () -> ()) {
+        
+        // weak self - prevent retain cycles
+        apiService.getPopularMoviesData { [weak self] (result) in
+            
+            switch result {
+            case .success(let listOf):
+                self?.popularMovies = listOf.movies
+                completion()
+            case .failure(let error):
+                // Something is wrong with the JSON file or the model
+                print("Error processing json data: \(error)")
+            }
+        }
+    }
+    
+    func numberOfRowsInSection(section: Int) -> Int {
+        if popularMovies.count != 0 {
+            return popularMovies.count
+        }
+        return 0
+    }
+    
+    func cellForRowAt (indexPath: IndexPath) -> Movie {
+        return popularMovies[indexPath.row]
+    }
+}
---
+++ Rest API/Rest API/Controller/MovieViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Rest API
+//
+//  Created by Sevda Gul Baran on 16.10.2022.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var viewModel = MovieLogic()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loadPopularMoviesData()
+    }
+    
+    private func loadPopularMoviesData() {
+        viewModel.fetchPopularMoviesData { [weak self] in
+            self?.tableView.dataSource = self
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+// MARK: - TableView
+extension MovieViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieTableViewCell
+        
+        let movie = viewModel.cellForRowAt(indexPath: indexPath)
+        cell.setCellWithValuesOf(movie)
+        
+        return cell
+    }
+}
